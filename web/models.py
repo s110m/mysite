b501a3d948c125acc67599f757fb64bc9302f24c
SedAli <s110mmail@gmail.com>
2020-05-16 02:25:58 +0330

    sahar ast

--- web/models.py
+++ web/models.py
@@ -2,13 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Token(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    token=models.CharField(max_length=45)
+    def __str__(self):
+        return '{}_token'.format(self.user)
+
+
+
 class Expense(models.Model):
     text=models.CharField(max_length=255)
     date=models.DateTimeField()
     amount=models.BigIntegerField()
     user=models.ForeignKey(User, on_delete=models.CASCADE)  #user is an object of class Userpythn
     def __str__(self):
-        return self.text
+        return '{}-{}'.format(self.text, self.date)
 
 
 class Income(models.Model):
--- web/views.py
+++ web/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
-
+#from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+#from django.http import HttpResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User, Token, Expense, Income
+from datetime import datetime
+#from django.utils import timezone
 # Create your views here.
+
+
+@csrf_exempt
+def submit_expense(request):
+    """ submit an expense """
+
+    # TODO: revise validation for the amount
+    # this_date = request.POST['date'] if 'date' in request.POST else timezone.now()
+    # this_text = request.POST['text'] if 'text' in request.POST else ""
+    # this_amount = request.POST['amount'] if 'amount' in request.POST else "0"
+    # this_token = request.POST['token'] if 'token' in request.POST else ""
+    # this_user = get_object_or_404(User, token__token=this_token)
+    #
+    # Expense.objects.create(user=this_user, amount=this_amount,
+    #                        text=this_text, date=this_date)
+    this_token=request.POST['token']
+    this_user=User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date=datetime.now()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=date)
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)  # return {'status':'ok'}
+
+
+@csrf_exempt
+def submit_income(request):
+    """ submit an income """
+
+    # TODO: revise validation for the amount
+    # this_date = request.POST['date'] if 'date' in request.POST else timezone.now()
+    # this_text = request.POST['text'] if 'text' in request.POST else ""
+    # this_amount = request.POST['amount'] if 'amount' in request.POST else "0"
+    # this_token = request.POST['token'] if 'token' in request.POST else ""
+    # this_user = get_object_or_404(User, token__token=this_token)
+    #
+    # Expense.objects.create(user=this_user, amount=this_amount,
+    #                        text=this_text, date=this_date)
+    this_token=request.POST['token']
+    this_user=User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date=datetime.now()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=date)
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)  # return {'status':'ok'}
